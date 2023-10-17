fe59d42f1ecf329e998f566ef376a86b5334013b
Darmin RC <darminrc@gmail.com>
2023-10-17 15:02:56 -0400

    Búsqueda - Scroll - Esqueleto de Carga

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -6,6 +6,16 @@
         <div class="total">
           <h4>Pacientes totales: {{ pacientes.length }}</h4>
         </div>
+        <div class="buscar">
+          <input
+            type="search"
+            v-model="filtroNombre"
+            placeholder="Buscar pacientes por nombre..."
+          />
+          <button @click="buscarPacientes">
+            <i class="fa fa-search" style="font-size: 1.3em"></i>
+          </button>
+        </div>
         <div class="seleccionar-todo">
           <input
             type="checkbox"
@@ -25,43 +35,62 @@
       <br />
       <div class="table">
         <table>
-          <tr>
-            <th>Nombre y Apellido:</th>
-            <th>Diagnóstico:</th>
-            <th>Teléfono</th>
-            <th>Edad</th>
-            <th>Medicamento</th>
-            <th>Recomendación</th>
-            <th>Editar</th>
-            <th>Eliminar</th>
-          </tr>
-          <tr v-for="paciente in pacientes" :key="paciente.id">
-            <td>{{ paciente.nombre }}</td>
-            <td>{{ paciente.diagnostico }}</td>
-            <td>{{ paciente.telefono }}</td>
-            <td>{{ paciente.edad }}</td>
-            <td>{{ paciente.medicamento }}</td>
-            <td>
-              {{ paciente.proceso ? "Tiene" : "No Tiene" }}
-            </td>
-            <td>
-              <button
-                style="color: var(--green)"
-                @click="editarPaciente(paciente)"
-              >
-                <i class="fa fa-edit"> </i>
-              </button>
-            </td>
-            <td>
-              <button
-                style="color: var(--red)"
-                @click="eliminarPaciente(paciente)"
-              >
-                <i class="fa fa-trash"></i>
-              </button>
-            </td>
+          <thead>
+            <tr>
+              <th>Nombre y Apellido:</th>
+              <th>Diagnóstico:</th>
+              <th>Teléfono</th>
+              <th>Edad</th>
+              <th>Medicamento</th>
+              <th>Recomendación</th>
+              <th>Editar</th>
+              <th>Eliminar</th>
+            </tr>
+          </thead>
+          <tfoot>
+            <tr v-for="paciente in pacientes" :key="paciente.id">
+              <td>{{ paciente.nombre }}</td>
+              <td>{{ paciente.diagnostico }}</td>
+              <td>{{ paciente.telefono }}</td>
+              <td>{{ paciente.edad }}</td>
+              <td>{{ paciente.medicamento }}</td>
+              <td>
+                {{ paciente.proceso ? "Tiene" : "No Tiene" }}
+              </td>
+              <td>
+                <button
+                  style="color: var(--green)"
+                  @click="editarPaciente(paciente)"
+                >
+                  <i class="fa fa-edit"> </i>
+                </button>
+              </td>
+              <td>
+                <button
+                  style="color: var(--red)"
+                  @click="eliminarPaciente(paciente)"
+                >
+                  <i class="fa fa-trash"></i>
+                </button>
+              </td>
+            </tr>
+          </tfoot>
+        </table>
+        <table v-if="cargando" class="tabla-carga">
+          <tr v-for="index in 10" :key="index">
+            <td class="esqueleto"></td>
+            <td class="esqueleto"></td>
+            <td class="esqueleto"></td>
+            <td class="esqueleto"></td>
+            <td class="esqueleto"></td>
+            <td class="esqueleto"></td>
+            <td class="esqueleto"></td>
+            <td class="esqueleto"></td>
           </tr>
         </table>
+        <br />
+
+        <span v-if="!cargando && sinResultados">¡Sin resultados!</span>
 
         <EditarPaciente
           v-if="mostrarFormularioEditar && pacienteSeleccionado"
@@ -97,6 +126,9 @@ export default {
       db: db,
       selectAll: false,
       selectedPacientes: [],
+      filtroNombre: "",
+      sinResultados: false,
+      cargando: true,
     };
   },
   components: {
@@ -120,6 +152,9 @@ export default {
       blanqueadora: doc.data().blanqueadora,
       proceso: doc.data().proceso,
     }));
+
+    // Indicar que la carga ha terminado
+    this.cargando = false;
   },
   methods: {
     async eliminarPaciente(paciente) {
@@ -183,6 +218,20 @@ export default {
         }
       });
     },
+    buscarPacientes() {
+      if (this.filtroNombre.trim() === "") {
+        // Restaurar la lista completa de pacientes si no hay filtro
+        this.actualizarListaPacientes();
+        this.sinResultados = false; // Reiniciar el estado de sinResultados
+        return;
+      }
+
+      // Filtrar pacientes por nombre
+      this.pacientes = this.pacientes.filter((paciente) =>
+        paciente.nombre.toLowerCase().includes(this.filtroNombre.toLowerCase())
+      );
+      this.sinResultados = this.pacientes.length === 0;
+    },
   },
 };
 </script>
@@ -204,9 +253,32 @@ export default {
   border-radius: 1.5em;
 }
 
+.buscar {
+  display: flex;
+  width: 70%;
+  height: 100%;
+}
+
+.buscar input {
+  width: 90%;
+  color: var(--white);
+  padding: 0.5em;
+  margin: 0.1em;
+  font-size: 1em;
+  background: var(--aqua-back);
+}
+
+.buscar button {
+  width: 10%;
+}
+
+.buscar input::placeholder {
+  color: #ddd;
+}
+
 .total,
 .seleccionar-todo {
-  width: 50%;
+  width: 30%;
   align-items: center;
   display: flex;
 }
@@ -228,10 +300,27 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
+  max-height: 95vh;
+  overflow-y: auto;
 }
+
+.table::-webkit-scrollbar {
+  width: 0.8em;
+  padding-left: 2em;
+}
+
+.table::-webkit-scrollbar-thumb {
+  background-color: var(--white);
+  border-radius: 0.4em;
+}
+
+.table::-webkit-scrollbar-thumb:hover {
+  background-color: #ddd;
+}
+
 .table table {
   border-collapse: collapse;
-  background: var(--aqua-back);
+  background: var(--aqua-input);
   border: solid 2px var(--white);
   text-align: center;
   font-size: 1em;
@@ -241,5 +330,26 @@ export default {
 th {
   border: solid 2px var(--white);
   padding: 0.7em;
+}
+
+.table thead th {
+  background: var(--aqua-back);
+}
+
+.tabla-carga {
+  width: 100%;
+  border-collapse: collapse;
+  border: 1px solid var(--white);
+}
+
+.tabla-carga table td,
+th {
+  padding: 0.7em;
+}
+
+.esqueleto {
+  height: 20px; /* Altura de fila de esqueleto */
+  background-color: var(--aqua-back); /* Color de fondo gris para esqueleto */
+  border-top: 1px solid var(--aqua-input);
 }
 </style>
